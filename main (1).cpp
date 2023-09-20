c9819c721c91b8a75308612e41fbf520c5e80715
ilya-bondarcuk0-2kx <144292229+ilya-bondarcuk0-2kx@users.noreply.github.com>
2023-09-20 14:50:39 +0300

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sum = 0;
+    for(int i =1;i<100;i++)
+    {
+        sum+=i;
+        if(sum > 1000)
+        {
+            cout<<sum<<endl;
+            break;
+        }
+    }
+}
---
+++ main (2).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 10;
+    for(x;x>0;x-=2)
+    {
+        cout<<x<<endl;
+    }
+}
