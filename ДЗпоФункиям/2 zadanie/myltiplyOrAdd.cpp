80ce2ae2405230eb5109a6d67b5a5a2c4ae110e0
ilya-bondarcuk0-2kx <144292229+ilya-bondarcuk0-2kx@users.noreply.github.com>
2023-10-13 23:09:12 +0300

    Add files via upload

---
+++ ДЗпоФункиям/2 zadanie/myltiplyOrAdd.cpp
@@ -0,0 +1,7 @@
+double myltiplyOrAdd(double value, double value2, bool add = true) {
+    if (add) {
+        return value + value2;
+    } else {
+        return value * value2;
+    }
+}
---
+++ ДЗпоФункиям/2 zadanie/myltiplyOrAdd.h
@@ -0,0 +1,1 @@
+double myltiplyOrAdd(double, double, bool = true);
