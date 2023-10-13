6d1344a10d6ecf8cb83578e365c6a0131e1ed26f
ilya-bondarcuk0-2kx <144292229+ilya-bondarcuk0-2kx@users.noreply.github.com>
2023-10-13 23:00:12 +0300

    main.cpp

--- ДЗпоФункиям/1 zadanie/main.cpp
+++ ДЗпоФункиям/1 zadanie/main.cpp
@@ -1,19 +1,9 @@
 #include <iostream>
-
+#include <multilyOrAdd.h>
 
 using namespace std;
 
-double myltiplyOrAdd(bool action , double value, double value2)
-{
-    if(action)
-    {
-       return value + value2;
-    }
-    else
-    {
-        return value * value2;
-    }
-}
+
 int main() {
     setlocale(LC_ALL, "");
     cout<<myltiplyOrAdd(true,2,3)<<endl;
